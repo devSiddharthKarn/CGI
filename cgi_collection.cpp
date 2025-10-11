434dd78d9674cdaecd00e0eaf34f7d6c619ecac6
devSiddharthKarn <192719686+devSiddharthKarn@users.noreply.github.com>
2025-10-11 21:15:47 +0545

    modified readme

--- cgi_collection.cpp
+++ cgi_collection.cpp
@@ -1,3 +1,12 @@
+// =============================================================
+//  CGI - C++ Graphics Ingine
+//  Simple. Effective. Elegant.
+//  Copyright (c) 2025 Siddharth Karn
+//  Licensed under the Apache License, Version 2.0
+//  See LICENSE file in the project root for full license information.
+// =============================================================
+
+
 #include <vector>
 #include <cstdlib>  // for rand()
 #include <ctime>    // for srand()
